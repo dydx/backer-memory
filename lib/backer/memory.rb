b1ea3bb8f875d3324fbdd1104ca53c6a316916d6
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-01-03 22:57:53 -0500

    bleeh

--- lib/backer/memory.rb
+++ lib/backer/memory.rb
@@ -1,31 +1,33 @@
 require "backer/memory/version"
 
 module Backer
-  class Memory
-    def initialize
-      @records = {}
-      @id = 1
-    end
-
-    def self.model_class(model)
-      define_method(:model_class) do
-        instance_variable_set(:@model_class, model)
+  module Memory
+    class Base
+      def initialize
+        @records = {}
+        @id = 1
       end
-    end
 
-    def new(attributes = {})
-      model_class.new(attributes)
-    end
+      def self.model_class(model)
+        define_method(:model_class) do
+          instance_variable_set(:@model_class, model)
+        end
+      end
 
-    def save(object)
-      object.id = @id
-      @records[@id] = object
-      @id += 1
-      return object
-    end
+      def new(attributes = {})
+        model_class.new(attributes)
+      end
 
-    def find_by_id(n)
-      @records[n.to_i]
+      def save(object)
+        object.id = @id
+        @records[@id] = object
+        @id += 1
+        return object
+      end
+
+      def find_by_id(n)
+        @records[n.to_i]
+      end
     end
   end
 end
